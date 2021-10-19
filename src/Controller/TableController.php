c0fb6c2eb9c00ffcd1584d6c0a95f5196b7f1d76
JENOUVRIER Hugo <jenohug@ndlp.fr>
2021-10-19 16:05:45 +0200

    Add form

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TableChoiceType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
 * @Route("/table", name="table")
@@ -12,14 +14,54 @@ use Symfony\Component\Routing\Annotation\Route;
 class TableController extends AbstractController
 {
     /**
+    * @Route("/select", name="table_select") 
+    */
+    public function select($request){
+
+        $form = $this->createForm(TableChoiceType::class); 
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $data = $form->getData();
+            $n = $data['table_number'];
+            $m = $data['lines_count'];
+            $color = $data['color'];
+        $response = $this->render('table/index.html.twig', [
+            'controller_name' => 'TableController',
+            'n' => $n,
+            'm' => $m,
+            'color' => $color,
+        ]);
+    } else {
+            $response = $this->render('table/vue.html.twig',[
+                'formulaire' => $form->createView(),
+            ]);
+        }
+    }
+
+
+    /**
      * @Route("/print/{n}/{m}", name="table_print")
      */
-    public function index(int $n, int $m): Response
+    public function index(int $n, int $m, Request $request): Response
     {
+        if ($form->isSubmitted()){
+            data($form->getData());
+            $n = $data['table_number'];
+            $m = $data['lines_count'];
+            $color = $data['color'];
+        $color = $request->get('c');
         return $this->render('table/index.html.twig', [
             'controller_name' => 'TableController',
             'n' => $n,
             'm' => $m,
+            'color' => $color,
         ]);
+    } else {
+            $response = $this->render('table/vue.html.twig',[
+                'formulaire' => $form->createView(),
+            ]);
+        }
     }
 }
